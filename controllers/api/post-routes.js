b45f7ac7304f340058ded037eb3fde0363a016c4
xunvyre <victoria@thedavids.us>
2022-09-25 16:33:09 -0400

    post fetch, create, delete, edit hooked up and tested

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -5,19 +5,10 @@ router.get('/', (req, res) =>
 {
     Post.findAll
     ({
-        attributes: ['id', 'user_id', 'title', 'entry', 'updated_at'],
+        attributes: ['id', 'user_id', 'title', 'post_text', 'updated_at'],
         order: [['updated_at', 'DESC']],
         include:
         [
-            {
-                model: Image,
-                attributes: ['id', 'type', 'name', 'data', 'user_id', 'post_id', 'created_at'],
-                include:
-                {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
             {
                 model: Comment,
                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
@@ -46,7 +37,7 @@ router.get('/:id', (req, res) =>
     Post.findOne
     ({
         where: {id: req.params.id},
-        attributes: ['id', 'title', 'entry', 'created_at'],
+        attributes: ['id', 'title', 'post_text', 'created_at'],
         include:
         [
             {
@@ -70,6 +61,65 @@ router.get('/:id', (req, res) =>
         {
             res.status(404).json({message: `No posts under this ID.`});
             return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err =>
+    {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) =>
+{
+    Post.create
+    ({
+        title: req.body.title,
+        post_text: req.body.post_text,
+        user_id: req.body.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err =>
+    {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.put('/:id', (req, res) =>
+{
+    Post.update
+    (
+        { title: req.body.title, post_text: req.body.post_text },
+        { where: {id: req.params.id} }
+    )
+    .then(dbPostData =>
+        {
+            if (!dbPostData)
+            {
+                res.status(404).json({message: 'No post under this ID.'});
+                return;
+            }
+            res.json(dbPostData);
+        })
+    .catch(err =>
+        {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//delete a post
+router.delete('/:id', (req, res) =>
+{
+    Post.destroy({where: {id: req.params.id}})
+    .then(dbPostData =>
+    {
+        if (!dbPostData)
+        {
+          res.status(404).json({message: 'No post under this ID.'});
+          return;
         }
         res.json(dbPostData);
     })
--- models/Post.js
+++ models/Post.js
@@ -20,14 +20,10 @@ Post.init
             type: DataTypes.STRING,
             allowNull: false
         },
-        post_url:
+        post_text:
         {
             type: DataTypes.STRING,
             allowNull: false,
-            validate:
-            {
-                isURL: true
-            }
         },
         user_id:
         {
