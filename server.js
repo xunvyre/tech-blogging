612af07de6e277ee9f155a52522876699138aa26
xunvyre <victoria@thedavids.us>
2022-10-20 22:42:22 -0400

    reconfigured server.js based on feedback

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
-const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
+const express = require('express');
 const session = require('express-session');
-const sqlzStore = require('connect-session-sequelize')(session.Store);
-const hbs = exphbs.create({helpers});
-require('dotenv').config();
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const sqlzStore = require('connect-session-sequelize')(session.Store);
+
 const sess = 
 {
     secret: process.env.secret,
@@ -20,16 +18,20 @@ const sess =
     store: new sqlzStore({db: sequelize})
 };
 
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
-app.use(routes);
+app.use(require('./controllers'));
 
 // turn on connection to db and server
 sequelize.sync({force: false}).then(() =>
