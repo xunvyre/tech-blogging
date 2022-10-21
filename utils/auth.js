adc8a5c6f4b0b912806e0045486f742579285325
xunvyre <victoria@thedavids.us>
2022-10-20 22:15:31 -0400

    removed authorization for heroku

--- utils/auth.js
+++ utils/auth.js
@@ -1,13 +1,6 @@
 const withAuth = (req, res, next) =>
 {
-    if (!req.session.user_id)
-    {
-        res.redirect('/login');
-    }
-    else
-    {
-        next();
-    }
+    next();
 };
   
   module.exports = withAuth;
